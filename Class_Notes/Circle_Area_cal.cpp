e733f36cb3f91a1db8662140e48d749c8d6ca63e
MrSigmA-TW <andy20161515@gmail.com>
2025-02-26 00:38:15 +0800

    修改"課堂筆記"資料夾名為"Class_Notes" 完成Circle_Area_cal.cpp檔案

---
+++ Class_Notes/Circle_Area_cal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double r;
+    cin >> r;
+    cout << fixed << setprecision(10) << r * r * 3.14159 << endl;
+    return 0;
+}
+
