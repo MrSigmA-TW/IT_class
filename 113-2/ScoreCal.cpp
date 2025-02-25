19f2cf186ecdacbef0e98ecc87d02c7714bc9988
ΣR.SigmA <andy20161515@gmail.com>
2025-02-25 14:36:57 +0800

    2025/02/25 Update

---
+++ 113-2/ScoreCal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int CS; //chinses score
+   int ES; //english score
+   int total;
+   CS = 89;
+   ES = 85;
+   total = CS + ES;
+   cout << "Total score is " << total << endl;
+    return 0;
+}
