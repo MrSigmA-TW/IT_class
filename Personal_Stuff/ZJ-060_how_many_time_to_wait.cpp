d3343d8bbc6529b99497c1856f77ad47c847aa3e
Σr.SigmA <andy20161515@gmail.com>
2025-03-18 16:14:21 +0800

    新增多個C++程式檔案及更新VSCode設定

---
+++ Personal_Stuff/ZJ-060_how_many_time_to_wait.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int m;
+    cin >> m;
+    switch (m) {
+        case (0 < m || m < 25) :
+            cout << 25 - m << endl;
+            break;
+        case (25 < m || ) :
+    }
+    return 0;
+}
