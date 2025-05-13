f348fe4002f13816a89b44078481c9520083d4e0
ΣR.SigmA <andy20161515@gmail.com>
2025-05-13 16:15:58 +0800

    新增 2025_05_13-exam 的問題 A、B、C、C-2、D 和 F 檔案，實現基本的數學運算和排序功能

---
+++ Basic_Programming_and_Competitions/2025_05_13-exam/problem C-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    int n1 = 0;
+    int n2 = 0;
+    int n3 = 0;
+    cin >> N;
+    int k[N];
+    int O[N];
+    for (int i = 0; i < N; i++) {
+        cin >> k[i];
+    }
+    for (int i = 0; i < N; i++) {
+        if (k[i] == 1) n1++;
+        if (k[i] == 2) n2++;
+        if (k[i] == 3) n3++;
+    }
+    for (int i = 0; i < n1; i++) {
+        O[i] = 1;
+    }
+    for (int i = n1; i <= (n1 + n2 - 1); i++) {
+        O[i] = 2;
+    }
+    for (int i = (n1 + n2); i < N; i++) {
+        O[i] = 3;
+    }
+    for (int i = 0; i < N; i++) {
+        cout << O[i] << ' ';
+    }
+    return 0;
+}
---
+++ Basic_Programming_and_Competitions/2025_05_13-exam/problem D.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N, x, y, nx, ny;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> x >> y;
+        if ((x - 2) % 3 == 0) {
+            nx = (x - 2) / 3;
+        } else {
+            nx = (x - 2) / 3 + 1;
+        }
+        if ((y - 2) % 3 == 0) {
+            ny = (y - 2) / 3;
+        } else {
+            ny = (y - 2) / 3 + 1;
+        }
+        cout << nx * ny << endl;
+    }
+    return 0;
+}
