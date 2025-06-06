daec3e0a84f50efc6ffbd653534c54737ce6a1bc
ΣR.SigmA <andy20161515@gmail.com>
2025-06-07 02:21:27 +0800

    完成報告程式碼設計

--- Basic_Programming_and_Competitions/Source_Codes/presentation.cpp
+++ Basic_Programming_and_Competitions/Source_Codes/presentation.cpp
@@ -1,21 +1,44 @@
+#include <cmath>
+#include <iomanip>
 #include <iostream>
-
 using namespace std;
 
-class area {
-   private:
-    /* data */
-   public:
-    float generate_percent = 0.5;
-    float goods_A = 0.1;
-    float goods_B = 0.3;
-    float goods_C = 0.6;
-};
+int ABS(double a){
+    if (a < 0.0) return -1 * a;
+    if (a > 0.0) return a;
+}
 
 int main() {
-    int n;
-    cin >> n;
-    area k,l,m;
-    cout << n * k.generate_percent << ' ' << n * l.generate_percent << ' ' << n * m.generate_percent;
+    int n, k;
+    cin >> n >> k;
+    double total_armor = 0.0;
+    double total_score = 0.0;
+    bool isError = false;
+    for (int i = 0; i < n; i++) {
+        long long m;
+        double a, b, c, d;
+        cin >> m >> a >> b >> c >> d;
+        double sum = a + b + c + d;
+        if (ABS(sum - 1.0) > 1e-6) {
+            isError = true;
+            break;
+        }
+        total_score += m * (a * 4 + b * 3 + c * 2 + d * 1);
+        total_armor += m;
+    }
+    if (isError || total_armor == 0) {
+        cout << "ERR0R" << endl;
+    } else {
+        cout << fixed << setprecision(k) << total_score / total_armor << endl;
+    }
     return 0;
 }
+
+/*
+n代表有幾個興趣點(0<n<2148473647)
+每行先輸入m(每個興趣點預計生成幾個護甲 0<m<2148473647)
+a, b ,c ,d 分別代表這個興趣點金甲、紫甲、藍甲、白甲的生成機率
+4, 3, 2, 1 分別代表每個金甲、紫甲、藍甲、白甲的稀有度權重
+請幫地圖設計師計算出整張地圖的平均稀有度(精確到小數點第k位)
+如果無法計算或機率總和不為1，請直接輸出"ERR0R"
+*/
