0c7c1de906805007a304f3d91606ad9216f90431
ΣR.SigmA <andy20161515@gmail.com>
2025-06-08 00:53:20 +0800

    edited

--- Basic_Programming_and_Competitions/Source_Codes/presentation.cpp
+++ Basic_Programming_and_Competitions/Source_Codes/presentation.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-int ABS(double a){
+int ABS(double a) {
     if (a < 0.0) return -1 * a;
     if (a > 0.0) return a;
 }
@@ -29,16 +29,17 @@ int main() {
     if (isError || total_armor == 0) {
         cout << "ERR0R" << endl;
     } else {
-        cout << fixed << setprecision(k) << total_score / total_armor << endl;
+        cout << fixed << setprecision(5) << total_score / total_armor << endl;
     }
     return 0;
 }
 
 /*
+k代表有接下來有幾張地圖需要計算
 n代表有幾個興趣點(0<n<2148473647)
 每行先輸入m(每個興趣點預計生成幾個護甲 0<m<2148473647)
 a, b ,c ,d 分別代表這個興趣點金甲、紫甲、藍甲、白甲的生成機率
 4, 3, 2, 1 分別代表每個金甲、紫甲、藍甲、白甲的稀有度權重
-請幫地圖設計師計算出整張地圖的平均稀有度(精確到小數點第k位)
-如果無法計算或機率總和不為1，請直接輸出"ERR0R"
+請幫地圖設計師計算出整張地圖的平均稀有度(精確到小數點後第10位)
+如果無法計算，請直接輸出"ERR0R"
 */
